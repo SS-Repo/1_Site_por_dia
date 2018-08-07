9ab1b78911b6f7feb2b4050106218d300e5925e0
Sergio <sergio.desenvolvedor.web@gmail.com>
2018-08-07 18:05:25 -0300

    mais um dia enviando

---
+++ Interactive Buttons/jsons.js
@@ -0,0 +1,57 @@
+	//button 1
+	var button1 = document.querySelector('.btn1');
+ 	button1.addEventListener('click',function(){
+	var audio1 = document.querySelector('.son1');	
+	audio1.play();	
+	});
+	//
+	//button 2
+	var button2 = document.querySelector('.btn2');
+	button2.addEventListener('click',function(){
+		var audio2 = document.querySelector('.son2');	
+		audio2.play();	
+	});
+	//
+	//button 3
+	var button3 = document.querySelector('.btn3');
+	button3.addEventListener('click',function(){
+		var audio3 = document.querySelector('.son3');	
+		audio3.play();	
+	});
+	//
+	//button 4
+	var button4 = document.querySelector('.btn4');
+	button4.addEventListener('click',function(){
+		var audio4 = document.querySelector('.son4');	
+		audio4.play();	
+	});
+	//
+	//button 5
+	var button5 = document.querySelector('.btn5');
+	button5.addEventListener('click',function(){
+		var audio5 = document.querySelector('.son5');	
+		audio5.play();	
+	});
+	//
+	//button 6
+	var button6 = document.querySelector('.btn6');
+	button6.addEventListener('click',function(){
+		var audio6 = document.querySelector('.son6');	
+		audio6.play();	
+	});
+	//
+	//button 7
+	var button7 = document.querySelector('.btn7');
+	button7.addEventListener('click',function(){
+		var audio7 = document.querySelector('.son7');	
+		audio7.play();	
+	});
+	//
+	//button 8
+	var button8 = document.querySelector('.btn8');
+	button8.addEventListener('click',function(){
+		var audio8 = document.querySelector('.son8');	
+		audio8.play();	
+	});
+	//
+	
